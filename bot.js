f0927b0db8e6a663b69d6484cf3f7f7acf1139b4
metabdream70-commits <metabdream70@gmail.com>
2026-05-18 22:15:53 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -8,4 +8,5 @@ client.once('ready', () => {
   console.log('Bot is online!');
 });
 
-client.login('MTUwNTk5Njg0NTE4NzMzNDE5NA.GeR_D0.5xX5kbEP7oQWC6GWLHQU_skexwpr-wBsX1LMss');
+client.login(process.env.DISCORD_TOKEN);
+
